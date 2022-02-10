ec310059f4fcf8b8dca8cd729a9b6a29f632e3ea
aakrut dabhi <aakrutdabhi@gmail.com>
2022-02-11 00:21:50 +0530

    voice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
-function App() {
+import useStyles from "./styles";
+import wordsToNumbers from "words-to-numbers";
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2 ? wordsToNumbers(number) : number;
+
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("please try that again");
+          } else if (number) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          alt="alan ai"
+          className={classes.alanlogo}
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
